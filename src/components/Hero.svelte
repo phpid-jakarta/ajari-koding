c3e805a8da45c1029534fee08c88099f2627040a
Irfan Maulana <mazipanneh@gmail.com>
2020-06-25 00:10:55 +0700

    Update Hero.svelte

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,7 +1,7 @@
 <header class="container-fluid bg-dark pb-5 pt-5">
 	<div class="row justify-content-center align-items-center">
 		<div class="col-md-6 col-xs-12">
-			<h1 class="display-1 text-light">Ajari-Koding</h1>
+			<h1 class="display-1 text-light">Ajari Koding</h1>
 			<h2 class="text-muted">oleh PHPID</h2>
 			<p class="mt-3 text-light">Kumpulan berbagai sumber daya untuk belajar koding hasil karya para kreator lokal yang terpercaya dan telah dikurasi oleh komunitas PHPID</p>
 
