f95165c0cef7be3c44fad66c7f87752b74ee56de
Irfan Maulana <maulana.irfan@tokopedia.com>
2020-06-24 23:13:00 +0700

    fix: publicPath 🔥

---
+++ publicPathReplacer.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const indexHtmlPath = path.resolve(`./public/index.html`);
+const publicPath = 'ajari-koding';
+
+const main = async () => {
+  try {
+    const htmlContent = await fs.readFileSync(indexHtmlPath, {
+      encoding: 'utf-8',
+		});
+		const fixedContent = htmlContent
+			.replace(`/favicon.png`, `/${publicPath}/favicon.png`)
+			.replace(`/build/bundle.css`, `/${publicPath}/build/bundle.css`)
+			.replace(`/build/bundle.js`, `/${publicPath}/build/bundle.js`);
+
+		fs.writeFile(indexHtmlPath, fixedContent,
+      function (err) {
+        if (err) {
+          return console.log('❌ Error write file index.html', err);
+        }
+        console.log('✅ Success write file index.html');
+      }
+		);
+  } catch (error) {
+    console.error('❌ Error read file html', error);
+  }
+};
+
+main();
