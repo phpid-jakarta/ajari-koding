7008c7d28e220b551b7a5ed17811ce230eab07e8
Irfan Maulana <mazipanneh@gmail.com>
2020-06-26 17:55:40 +0700

    Update Hero.svelte

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -17,7 +17,7 @@
           href="https://github.com/phpid-jakarta/ajari-koding"
           target="_blank"
           rel="noopener noreferrer"
-          class="btn btn-outline-light">
+          class="btn btn-outline-light mb-2 mr-2">
 					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-square"><polyline points="9 11 12 14 22 4"></polyline><path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path></svg>
 					Yuk bantu kami
 				</a>
@@ -25,7 +25,7 @@
           href="https://github.com/phpid-jakarta/ajari-koding/issues/new?assignees=%40mazipan&labels=data-proposal&template=add_new_data.md&title=New+Data+-+Nama+Produk"
           target="_blank"
           rel="noopener noreferrer"
-          class="btn btn-light">
+          class="btn btn-light mb-2">
 					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
           Tambahkan data baru
         </a>
