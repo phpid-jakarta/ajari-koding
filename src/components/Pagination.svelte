e9ed61152bb79eb2be43adf3ff3f41f0dc9fa4fb
Irfan Maulana <mazipanneh@gmail.com>
2020-06-27 20:58:19 +0700

    Update Pagination.svelte

--- src/components/Pagination.svelte
+++ src/components/Pagination.svelte
@@ -4,7 +4,7 @@
 
   const dispatch = createEventDispatcher();
   const winWidth = window.innerWidth;
-  const perPageOptions = winWidth <= 767 ? [10, 20, 40, 60] : [9, 15, 30, 60];
+  const perPageOptions = winWidth <= 767 ? [20, 30, 50, 100] : [18, 30, 60, 90];
 
   export let total;
   export let perPage;
@@ -35,7 +35,10 @@
 <div class="d-flex justify-content-center mt-5">
   <div class="dropdown align-self-baseline mr-2">
     <button use:clickOutside class="btn btn-primary" on:click={ () => {showPerPageOptions = !showPerPageOptions} } on:click_outside={ () => {showPerPageOptions = false}}>
-      Per Halaman: {perPage} ▾
+      <span>{perPage}</span>
+<svg width="18" height="18" viewBox="0 0 16 16" class="ml-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+</svg>
     </button>
     <ul class="dropdown-menu {showPerPageOptions ? 'show' : ''}">
       {#each perPageOptions as page (page)}
